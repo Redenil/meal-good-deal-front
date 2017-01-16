17b6de08449598a501041c69b9bddf9c257ff420
Abdellah Boussaidi <aboussaidi@aboussaidi-ltm.internal.salesforce.com>
2017-01-16 01:44:32 +0100

    Add facebook Auth Android and iOS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component, ViewChild } from '@angular/core';
 import { Nav, Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 import { TabsPage } from '../pages/tabs/tabs';
+import { LoginPage } from '../pages/login/login';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
-  rootPage: any = TabsPage;
+  rootPage: any = LoginPage;
 
   constructor(public platform: Platform) {
     this.initializeApp();
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,18 +1,23 @@
-import { Component } from '@angular/core';
+import { Component ,OpaqueToken, Injectable, Inject} from '@angular/core';
 import { NavController, NavParams, LoadingController } from 'ionic-angular';
-import { NativeStorage, TwitterConnect } from 'ionic-native';
-import { DealsPage } from '../pages'
+import { NativeStorage, TwitterConnect, Facebook } from 'ionic-native';
+import { APP_CONFIG_TOKEN, APP_CONFIG, ApplicationConfig } from '../../app/app-config';
+import { DealsPage } from '../pages';
 
 @Component({
   selector: 'page-login',
-  templateUrl: 'login.html'
+  templateUrl: 'login.html',
+  providers: [{ provide: APP_CONFIG_TOKEN, useValue: APP_CONFIG }]
 })
 export class LoginPage {
-
+  private facebookAppId: number;
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    public loadingCtrl: LoadingController) { }
+    public loadingCtrl: LoadingController,
+    @Inject(APP_CONFIG_TOKEN) config: ApplicationConfig) {
+      Facebook.browserInit(config.facebookAppId, "v2.8");
+    }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
@@ -44,6 +49,47 @@ export class LoginPage {
       }, function (error) {
         loading.dismiss();
       });
+    });
+  }
+
+  loginFacebook(){
+    let nav = this.navCtrl;
+    let loading = this.loadingCtrl.create({
+      content: 'Please wait...'
+    });
+
+    loading.present();
+
+    let permissions = new Array<string>();
+
+    //Facebook permission that app need
+    permissions = ["public_profile"];
+    Facebook.login(permissions)
+    .then(function(response){
+      let userId = response.authResponse.userID;
+      let params = new Array<string>();
+      //Getting name and gender properties
+      Facebook.api("/me?fields=name,gender", params)
+      .then(function(user) {
+        user.picture = "https://graph.facebook.com/" + userId + "/picture?type=large";
+        //Save user info in the NativeStorage
+        NativeStorage.setItem('facebook_user',
+        {
+          name: user.name,
+          gender: user.gender,
+          picture: user.picture
+        })
+        .then(function(){
+          loading.dismiss();
+          nav.push(DealsPage);
+        }, function (error) {
+          console.log(error);
+          loading.dismiss();
+        })
+      })
+    }, function(error){
+      console.log(error);
+      loading.dismiss();
     });
   }
 }
