53694dc40e73d3d8a3fe90156dda3e22ff5bd695
raouafi <raouafi@talentsoft.com>
2017-01-17 18:54:15 +0100

    Refacto => Extracting twitter login in service #missingPart

--- src/services/userProfile/userProfile.model.ts
+++ src/services/userProfile/userProfile.model.ts
@@ -4,7 +4,21 @@ export class UserProfile {
     public picture: any;
     public profileType: ProfileType;
 
-    constructor() {
+    public facebookToken: string;
+    public twitterToken: string;
+
+    constructor(userName: string = null,
+        name: string = null,
+        picture: any = null,
+        profileType: ProfileType = null,
+        facebookToken: string = null,
+        twitterToken: string = null) {
+        this.userName = userName;
+        this.name = name;
+        this.picture = picture;
+        this.profileType = profileType;
+        this.facebookToken = facebookToken;
+        this.twitterToken = twitterToken;
     }
 }
 
