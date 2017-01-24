1eae781bc97b3ba2f7c479df47e2de792f9f28fa
Abdellah Boussaidi <aboussaidi@aboussaidi-ltm.internal.salesforce.com>
2017-01-24 23:33:19 +0100

    update @ionic/app-scripts: 1.0.0

--- src/helpers/configHelper.ts
+++ src/helpers/configHelper.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ConfigHelper {
     public configurations:any;
-    constructor() {      
+    constructor() {
         this.configurations = require('./config.json');
     }
 }
